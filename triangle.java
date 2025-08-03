a46fff23399c24ae92d0b6e13559edc5245b6964
LavanyaS2411 <lavanyaskanna11@gmail.com>
2025-08-03 21:15:14 +0530

    Add files via upload

---
+++ triangle.java
@@ -0,0 +1,18 @@
+package PSNA;
+
+public class triangle {
+
+	public static void main(String[] args) {
+		int rows = 5; // number of rows in the triangle
+
+        for (int i = 1; i <= rows; i++) {  
+            for (int j = 1; j <= i; j++) {  
+                System.out.print("* ");  
+            }
+            System.out.println(); 
+        }
+        
+
+	}
+
+}
